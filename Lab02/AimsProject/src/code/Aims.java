495f9dc2c26bce519b2af9a1afd866ea223c0893
Star-Kaze <91308498+Star-Kaze@users.noreply.github.com>
2024-10-24 22:52:15 +0700

    Add files via upload

---
+++ Lab02/AimsProject/src/code/Aims.java
@@ -0,0 +1,24 @@
+package code;
+
+public class Aims {
+    public static void main(String[] args) {
+        Cart anOrder = new Cart();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("afsdfs", "adsfa", "adfsdfsdf", 89, 9.78f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("afsdfs", "adsfa", "adfsdfsdf", 89, 7.78f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("afsdfs", "adsfa", "adfsdfsdf", 89, 8.78f);
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("afsdfs", "adsfa", "adfsdfsdf", 89, 4.78f);
+        DigitalVideoDisc dvd5 = new DigitalVideoDisc("afsdfs", "adsfa", "adfsdfsdf", 89, 5.78f);
+        DigitalVideoDisc dvd6 = new DigitalVideoDisc("afsdfs", "adsfa", "adfsdfsdf", 89, 2.78f);
+        anOrder.addDigitalVideoDisc(dvd1);
+        anOrder.addDigitalVideoDisc(dvd2);
+        anOrder.addDigitalVideoDisc(dvd3);
+        anOrder.addDigitalVideoDisc(dvd4);
+        anOrder.addDigitalVideoDisc(dvd5);
+        anOrder.addDigitalVideoDisc(dvd6);
+        anOrder.removeDigitalVideoDisc(dvd6);
+        System.out.println(anOrder.totalCost());
+        System.out.println(anOrder.getQtyOrdered());
+    }
+
+
+}
