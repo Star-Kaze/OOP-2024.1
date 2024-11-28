367f53301344d27fd58d6756839070ba8607d65c
LamLam <nguyenbaolamss030205@gmail.com>
2024-11-28 16:57:51 +0700

    first commit

---
+++ Lab02/AimsProject/src/code/Cart.java
@@ -0,0 +1,47 @@
+package code;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private List<DigitalVideoDisc> itemsOdered;
+    private int qtyOrdered;
+
+    public Cart() {
+        itemsOdered = new ArrayList<>();
+        qtyOrdered = 0;
+    }
+
+    public int getQtyOrdered() {
+        return qtyOrdered;
+    }
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if(qtyOrdered < MAX_NUMBERS_ORDERED) {
+            itemsOdered.add(disc);
+            qtyOrdered++;
+            System.out.println("The disc has been added!");
+        } else {
+            System.out.println("The cart is almost full!");
+        }
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        if(itemsOdered.contains(disc)) {
+            itemsOdered.remove(disc);
+            qtyOrdered--;
+            System.out.println("The disc has been removed!");
+        } else {
+            System.out.println("The cart do not have this disc!");
+        }
+    }
+
+    public double totalCost() {
+        double totalCost = 0;
+        for(DigitalVideoDisc disc:itemsOdered){
+            totalCost += disc.getCost();
+        }
+        return totalCost;
+    }
+}
